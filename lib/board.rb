ef44ac2bd55ffd731ce66a2b7878b94ac7476b97
trustpizza <maxelolsson@gmail.com>
2022-10-03 17:50:44 -0500

    Create good looking chess board

--- lib/board.rb
+++ lib/board.rb
@@ -5,14 +5,14 @@ class Board
   
   def initialize
     @grid = [
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
-      [white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square]
+      [8, white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
+      [7, black_square, white_square, black_square, white_square, black_square, white_square, black_square, white_square],
+      [6, white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
+      [5, black_square, white_square, black_square, white_square, black_square, white_square, black_square, white_square],
+      [4, white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
+      [3, black_square, white_square, black_square, white_square, black_square, white_square, black_square, white_square],
+      [2, white_square, black_square, white_square, black_square, white_square, black_square, white_square, black_square],
+      [1, black_square, white_square, black_square, white_square, black_square, white_square, black_square, white_square],
     ]
   end
 
@@ -20,6 +20,7 @@ class Board
     @grid.each do |row|
       puts row.join('')
     end
+    puts "  a b c d e f g h"
   end
     
 end
