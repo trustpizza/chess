8e2abbe113f5a32dc9a00efa53b6cc3bb1835f6f
trustpizza <maxelolsson@gmail.com>
2022-10-04 13:02:01 -0500

    Add allowed next moves method

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,5 +1,5 @@
 
-
+require 'pry-byebug'
 class Pawn
   attr_reader :symbol, :placement
 
@@ -9,7 +9,25 @@ class Pawn
     @placement = location
   end
 
+  def next_moves(out = [])
+    moves = [[1,0]]
+
+    moves.map do |move|
+      x = @placement[0] + move[0]
+      y = @placement[1] + move[1]
+      
+      out << [x,y] if x.between?(0,7) && y.between?(0,7)
+    end
+
+    out
+  end
+
+
+
   def update_placement(location)
     @placement = location
   end
 end
+
+binding.pry
+pawn = Pawn.new
