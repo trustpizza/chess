fbf375c951b645b81c97c42e30786c5287924ffb
trustpizza <maxelolsson@gmail.com>
2022-10-04 14:52:28 -0500

    Refactor #move_piece to incoporate pieces that move more than 1 spot

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 require_relative 'pieces/knight.rb'
 require_relative 'pieces/pawn.rb'
+require_relative 'pieces/rook.rb'
 require 'pry-byebug'
 
 class Board
@@ -11,19 +12,44 @@ class Board
   end
 
   def allowed_moves(piece)
-    allowed_moves = piece.next_moves
+    piece.next_moves
+  end
+
+  def moves_tree(piece, start, destination)
+    piece_class = piece.class
+    queue = [piece_class.new(start)]
+    current = queue.shift
+
+    until current.location == destination
+      current.next_moves.each do |move|
+        current.children << new_piece = piece_class.new(move, current)
+        queue << new_piece
+      end
+      current = queue.shift
+    end
+
+    current
   end
 
   def move_piece(piece, location)
     allowed_moves = allowed_moves(piece)
 
-    piece.set_location(location) if allowed_moves.include?(location)
-  end
+    if piece.is_a?(Knight) || piece.is_a?(Pawn)
+      piece.set_location(location) if allowed_moves.include?(location)
+    elsif piece.is_a?(Rook)
+      start = piece.location
+      piece.set_location(location) if moves_tree(piece, start, location)
+      binding.pry
 
+    end
+  end
 end
 
 game = Board.new
-binding.pry
+pawn = Pawn.new([1,0])
+rook = Rook.new([3,3])
+game.move_piece(rook, [3,0])
+
 game.pieces
 
 
---
+++ lib/pieces/rook.rb
@@ -0,0 +1,25 @@
+class Rook  
+    attr_accessor :children, :parent, :location
+
+    def initialize(location, parent = nil)
+        @location = location
+        @children = []
+        @parent = parent
+    end
+
+    def set_location(location)
+        @location = location
+    end
+
+    def next_moves(out = [])
+        moves = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        moves.map do |move|
+        x = @location[0] + move[0]
+        y = @location[1] + move[1]
+
+        out << [x, y] if x.between?(0, 7) && y.between?(0, 7)
+        end
+        out
+    end
+end
