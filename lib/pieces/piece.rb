9617d2e7f0015616bc018a53cddd746652c87e2f
trustpizza <maxelolsson@gmail.com>
2022-10-06 19:53:57 -0500

    Pseudocode began for invalid_move checker

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -13,14 +13,29 @@ class Piece
         @location = [rank, file]
     end
 
-    def find_possible_moves(board)
+
+
+    def possible_moves(board)
         moves = move_set.reduce([]) do |out, move|
             out << make_moves(board.grid, move[0], move[1]) #Function that takes the board, takes move[0] and move[1] to determine available moves
         end
         moves.compact.flatten(1)   
         binding.pry
-
     end
+
+    def invalid_moves(board, possible_moves)
+        
+        #To Check for invalid moves
+        # Search the board at every possible move for a piece
+        # If there is a piece there, and it is not valid (not yet worrying about color, checkmate, etc.)
+        # If there is a piece there all further moves along that branch are ALSO invalid
+
+        # Create an output arr called out
+        # Take the possible moves array and go through each of the [rank, file] pairings
+        # If any of the values 
+    end
+    private
+
 
     def make_moves(grid, rank_delta, file_delta)
         rank = location[0] + rank_delta
@@ -39,4 +54,6 @@ class Piece
     def valid_location?(rank, file)
         rank.between?(0,7) && file.between?(0,7)
     end
+
+
 end
