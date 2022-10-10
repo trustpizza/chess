9fc4dc92b753302586fb89b26e73053c75399294
trustpizza <maxelolsson@gmail.com>
2022-10-10 18:23:11 -0500

    Refactor update_board and update_location methods to do just that

--- lib/board.rb
+++ lib/board.rb
@@ -7,4 +7,14 @@ class Board
   def initialize(grid = Array.new(8) { Array.new(8) })
     @grid = grid
   end
+  #I should add rules to the game here! (Checkmate, turn-based, etc.)
+  def update_board(pieces)
+    pieces.each do |piece|
+      update_location(piece, piece.location)
+    end
+  end
+
+  def update_location(piece, coords)
+    @grid[coords[0]][coords[1]] = piece
+  end
 end
