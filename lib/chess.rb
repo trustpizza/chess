288da2ff422f04f336291ec309774bc9bc5f3308
trustpizza <maxelolsson@gmail.com>
2022-10-10 18:23:44 -0500

    Add new_game methods which creates all new pieces and updates the board

--- lib/chess.rb
+++ lib/chess.rb
@@ -19,9 +19,54 @@ class Chess
     end
 
     def new_game
+        @pieces = new_white_pieces + new_black_pieces
+
+        @board.update_board(pieces)
         binding.pry
-        Rook.new([0,0], board, 'color')
+    end
+
+    private
+
+    def new_white_pieces
+        out = []
+        for i in 0..7
+            pawn = Pawn.new([1, i], board, 'white')
+            out << pawn
+        end
+        rook1 = Rook.new([0,0], board, 'white')
+        rook2 = Rook.new([0,7], board, 'white')
+
+        knight1 = Knight.new([0,1], board, 'white')
+        knight2 = Knight.new([0,6], board, 'white')
+
+        bishop1 = Bishop.new([0,2], board, 'white')
+        bishop2 = Bishop.new([0,5], board, 'white')
+
+        queen = Queen.new([0,3], board, 'white')
+        king = King.new([0,4], board, 'white')
+        
+        out += [rook1, rook2, knight1, knight2, bishop1, bishop2, queen, king]
+    end
+
+    def new_black_pieces
+        out = []
+        for i in 0..7
+            pawn = Pawn.new([6, i], board, 'black')
+            out << pawn
+        end
+        rook1 = Rook.new([7,0], board, 'black')
+        rook2 = Rook.new([7,7], board, 'black')
+
+        knight1 = Knight.new([7,1], board, 'black')
+        knight2 = Knight.new([7,6], board, 'black')
+
+        bishop1 = Bishop.new([7,2], board, 'black')
+        bishop2 = Bishop.new([7,5], board, 'black')
+
+        queen = Queen.new([7,3], board, 'black')
+        king = King.new([7,4], board, 'black')
+        
+        out += [rook1, rook2, knight1, knight2, bishop1, bishop2, queen, king]
     end
 end
-
 Chess.new
