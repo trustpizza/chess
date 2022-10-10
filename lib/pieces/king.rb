13644dfe133c315e6ee6873b813dab5be58a9217
trustpizza <maxelolsson@gmail.com>
2022-10-10 14:40:04 -0500

    Add king.rb with moveset

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,5 +1,24 @@
 class King
-    attr_accessor 
-  
-
+  def initialize(location, board, color)
+    super(location, board, color)
   end
+
+  def move_set
+    [[0,1],[1,0],[-1,0],[0,-1], [1,1], [-1,1], [1,-1], [-1, -1]]
+  end
+
+  def valid_moves(possible_moves, board)
+      remove_same_colors(possible_moves, board)   
+  end
+
+  private 
+
+  def make_moves(grid, rank_delta, file_delta)
+    rank = location[0] + rank_delta
+    file = location[1] + file_delta
+    out = []
+    out << [rank, file] if valid_location?(rank,file)
+
+    out
+  end
+end
