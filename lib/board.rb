11856ba9af47ec8adb03d4dee39dee35c9e21214
trustpizza <maxelolsson@gmail.com>
2022-10-13 15:50:27 -0500

    Began to print board

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,10 @@
+require_relative "misc/misc.rb"
 require "pry-byebug"
 
 class Board 
   attr_accessor :grid, :pieces
+
+  include Misc
 
   def initialize(grid = Array.new(8) { Array.new(8) })
     @grid = grid
@@ -36,4 +39,44 @@ class Board
   def in_checkmate?(king)
     (king.valid_moves(king.possible_moves).empty? || king.valid_moves(king.possible_moves).all? { |km| check?(km) } ) && check?(king.location)
   end
+
+  def print_board
+    i = 1
+    puts "#{piece_or_square1([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+    puts "---+----+----+----+----+----+----+----"
+    puts "#{piece_or_square([i,0])} | #{piece_or_square([i,1])} | #{piece_or_square([i,2])} | #{piece_or_square([i,3])} | #{piece_or_square([i,4])} | #{piece_or_square([i,5])} | #{piece_or_square([i,6])} | #{piece_or_square([i,7])}"
+
+  end
+
+  def piece_or_square1(location)
+    return location unless @grid[location[0]][location[1]].nil?
+    
+    return white_square if location[1].even?
+    return black_square if location[1].odd?
+  end
+
+  def piece_or_square2(location)
+    return location unless @grid[location[0]][location[1]].nil?
+    
+    return white_square if location[1].odd?
+    return black_square if location[1].even?
+  end
 end
+
+board = Board.new
+#binding.pry
+board.print_board
