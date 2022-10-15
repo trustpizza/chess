fa5f26afae287c72a86c13f3fcddba05c5807a3f
trustpizza <maxelolsson@gmail.com>
2022-10-15 12:44:49 -0500

    Add translator to convert "b3" notation to numbers

---
+++ lib/translator.rb
@@ -0,0 +1,25 @@
+
+class Translator(letter_num)
+  
+  def initialize
+    @row = nil
+    @col = nil
+  end
+
+  def translate(letter_num)
+    coords = letter_num.split('')
+    translate_number(coords[1])
+    translate_row(coords[0])
+    { row: @row, col: @col }
+  end
+
+  private
+
+  def translate_letter(letter)
+    letter.downcase.ord - 97
+  end
+
+  def translate_number(number)
+    @row = 8 - number.to_i
+  end
+end
