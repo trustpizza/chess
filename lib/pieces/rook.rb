555230ae8856f61ebaa8c4dee05c061bbd59ea0a
trustpizza <maxelolsson@gmail.com>
2022-10-14 19:03:58 -0500

    finish rook class

---
+++ lib/pieces/rook.rb
@@ -0,0 +1,14 @@
+require_relative "pieces"
+
+class Rook < Piece
+    def initialize(board, args)
+        super(board, args)
+        @symbol = " \u265C "
+    end
+
+    private
+
+    def move_set
+        [[0, 1], [0, -1], [-1, 0], [1, 0]]
+    end
+end
